e4566c3d77dce6e72cc5318ebfa72d35c1e6b3d2
rastogiriya <rastogiriya081@gmail.com>
2024-01-15 18:42:18 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "./utils/api.js";
+
+function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
+  return <div className="App">App</div>;
+}
+
+export default App;
