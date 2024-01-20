0b76f42d9891666d020311b6ef2d8843d1c4c717
rastogiriya <rastogiriya081@gmail.com>
2024-01-20 13:33:39 +0530

    searchResult Page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,19 +4,26 @@ import { useParams } from "react-router-dom";
 import "./style.scss";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/videosSection";
+import Similar from "./carousels/Similar";
+import Recommendations from "./carousels/Recommendations";
+
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading: creditsLoading } = useFetch(
     `/${mediaType}/${id}/credits`
   );
-  console.log("Hello");
+  //console.log("Hello");
 
   return (
     <div>
       {/* <h1>riyaaaaaaaaaa</h1> */}
-      <DetailsBanner video={data?.result?.[0]} crew={credits?.crew} />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendations mediaType={mediaType} id={id} />
     </div>
   );
 };
