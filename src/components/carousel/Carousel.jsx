c2446691349acb73b5959f2358d5a858a5dd7c7f
Anubhav Pandey <akp99376@gmail.com>
2024-02-17 22:26:28 +0530

    bug fixes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -10,7 +10,7 @@ import dayjs from "dayjs";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/img";
 import PosterFallback from "../../assets/no-poster.png";
-import CircleRating from "../circleRating/Circlerating";
+import CircleRating from "../circleRating/CircleRating";
 import "./style.scss";
 import Genres from "../genres/Genres";
 
--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 
 import "./style.scss";
 import Img from "../lazyLoadImage/img";
-import CircleRating from "../circleRating/Circlerating";
+import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
