3c7c5dc0969a0b9efead537c4098d717cd103b03
Anubhav Pandey <akp99376@gmail.com>
2024-02-17 22:42:13 +0530

    bug fixes

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 
 import "./style.scss";
 import Img from "../lazyLoadImage/Img.jsx";
-import CircleRating from "../circleratingg/CircleRating.js";
+import CircleRating from "../circleratingg/CircleRating.jsx";
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
